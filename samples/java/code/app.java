d4e45f884d18ce818b76a493ba77d2bc78820846
Luis Carlos Aveiro <git@luisaveiro.com>
2022-07-12 21:25:32 +0100

    feat: created the Java sample.

---
+++ samples/java/code/app.java
@@ -0,0 +1,8 @@
+class App {
+    /**
+     * This method prints "Hello, World!" to stdout. 
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
