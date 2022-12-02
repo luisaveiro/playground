ccab0a63036d40ee496fac67ac41ddcef8f61536
Luis Carlos Aveiro <git@luisaveiro.com>
2022-12-02 21:26:45 +0000

    feat: created the Remotion sample.

---
+++ samples/remotion/installer.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env sh
+#
+# Main executable shell script.
+
+#######################################
+# Executes the Yarn `create` command to
+# create a new Remotion project using
+# Remotion starter kit.
+#
+# Arguments:
+#   directory.
+#######################################
+function installSample() {
+  cd "${1}" && yarn create video
+}
+
+#######################################
+# Move Remotion sample code to the app
+# directory.
+#
+# Arguments:
+#   directory.
+#######################################
+function moveSampleCode() {
+  local app_directory="${1}"
+
+  for directory in "${app_directory}"/*; do
+    if [[ -d "${directory}" ]]; then
+      _=$(mv "${directory}"/* "${app_directory}"/ >/dev/null 2>/dev/null)
+      _=$(mv "${directory}"/.* "${app_directory}"/ >/dev/null 2>/dev/null)
+
+      rm -r "${directory}"
+      break;
+    fi
+  done
+}
+
+#######################################
+# Main shell script.
+#
+# Outputs:
+#   Writes message to stdout.
+#######################################
+function main() {
+  local app_directory="/app"
+
+  installSample "${app_directory}"
+  moveSampleCode "${app_directory}"
+
+  echo "Remotion sample is ready!"
+}
+
+# Execute main function...
+main
