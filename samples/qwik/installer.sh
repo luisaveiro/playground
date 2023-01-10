9a1a9fc0a03c7f5551d36a64beab9969e9ca665e
Luis Carlos Aveiro <git@luisaveiro.com>
2023-01-10 17:17:03 +0000

    feat: created the Qwik App sample.

---
+++ samples/qwik/installer.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env sh
+#
+# Main executable shell script.
+
+#######################################
+# Fix in place to ensure Qwik sample
+# installs dependencies.
+#
+# Arguments:
+#   directory.
+#######################################
+function installDependencies() {
+  local app_directory="${1}"
+
+  if [ ! -d "${app_directory}/node_modules" ]; then
+    cd "${app_directory}" && yarn install
+  fi
+}
+
+#######################################
+# Executes the Yarn `create` command to
+# create a new Qwik project using
+# Qwik starter kit.
+#
+# Arguments:
+#   directory.
+#######################################
+function installSample() {
+  cd "${1}" && yarn create qwik
+}
+
+#######################################
+# Move Qwik sample code to the app
+# directory.
+#
+# Arguments:
+#   directory.
+#######################################
+function moveSampleCode() {
+  local app_directory="${1}"
+
+  for directory in "${app_directory}"/*; do
+    if [[ -d "${directory}" ]]; then
+      _=$(mv "${directory}"/* "${app_directory}"/ >/dev/null 2>/dev/null)
+      _=$(mv "${directory}"/.* "${app_directory}"/ >/dev/null 2>/dev/null)
+
+      rm -r "${directory}"
+      break;
+    fi
+  done
+}
+
+#######################################
+# Main shell script.
+#
+# Outputs:
+#   Writes message to stdout.
+#######################################
+function main() {
+  local app_directory="/app"
+
+  installSample "${app_directory}"
+  moveSampleCode "${app_directory}"
+  installDependencies "${app_directory}"
+
+  echo "Qwik sample is ready!"
+}
+
+# Execute main function...
+main
